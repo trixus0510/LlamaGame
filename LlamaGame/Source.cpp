1817a5339a60e6bab18381cc03e39bfd5287d26b
trixus0510 <91305142+trixus0510@users.noreply.github.com>
2022-12-25 18:21:53 -0500

    Update Source.cpp
    Creacion de la ventana y funcion setup

--- LlamaGame/Source.cpp
+++ LlamaGame/Source.cpp
@@ -8,3 +8,24 @@
 #include <time.h>
 #include <cmath>
 #include <math.h>
+
+
+
+void setup(void) {
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0.0, 2000, 0.0, 2000);
+}
+
+//Ventana
+int main(int argc, char** argv) {
+	srand(time(NULL));
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(1230, 650);
+	glutInitWindowPosition(50, 0);
+	glutCreateWindow("Llama Game");
+	setup();
+
+	glutMainLoop();
+}
