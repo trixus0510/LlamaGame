f35ca8d98e79748e0d24f1be9182c0bde266c125
trixus0510 <91305142+trixus0510@users.noreply.github.com>
2022-12-25 19:27:43 -0500

    Update Source.cpp
    LLamando funciones.

--- LlamaGame/Source.cpp
+++ LlamaGame/Source.cpp
@@ -218,5 +218,9 @@ int main(int argc, char** argv) {
 	glutCreateWindow("Llama Game");
 	setup();
 	glutDisplayFunc(render);
+	
+	glutKeyboardFunc(keyInput);
+	glutSpecialFunc(specialKeyInput);
+	
 	glutMainLoop();
 }
